d5db3f2b71532536ce281c2f9179bf6307ad0c0c
cramfull <tmdgud99@gmail.com>
2026-03-09 14:10:03 +0900

    feat: S14P21B203-75 #done [BE] Entity 작성

---
+++ backend/src/main/java/com/ssafy/ssarvis/bussinesscard/entity/BusinessCard.java
@@ -0,0 +1,52 @@
+package com.ssafy.ssarvis.bussinesscard.entity;
+
+import com.ssafy.ssarvis.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "business_cards")
+public class BusinessCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "card_name")
+    private String cardName;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "card_visibility")
+    private CardVisibility cardVisibility;
+
+    @NotNull
+    @Column(name = "view_count", columnDefinition = "UNSIGNED INT")
+    private Long viewCount;
+
+    @NotNull
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+}
---
+++ backend/src/main/java/com/ssafy/ssarvis/voice/entity/Voice.java
@@ -0,0 +1,45 @@
+package com.ssafy.ssarvis.voice.entity;
+
+import com.ssafy.ssarvis.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.type.SqlTypes;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "voices")
+public class Voice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(name = "order_id", columnDefinition = "BINARY(16)", unique = true)
+    private UUID modelUuid;
+
+    @NotNull
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
