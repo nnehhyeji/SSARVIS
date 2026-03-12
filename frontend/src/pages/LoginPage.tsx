2783813c938db1b7e361e153184f72ac1cd74f51
DESKTOP-6MRTL1B\SSAFY <eacxii1870@naver.com>
2026-03-12 10:36:37 +0900

    feat: S14P21B203-114 [INFRA] Test 스테이지에서 Lint로 분리

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,11 @@
 export default function LoginPage() {
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-            <h1 className="text-4xl font-bold text-green-600 mb-4">🔑 로그인 페이지</h1>
-            <p className="text-gray-600 mb-6">여기는 로그인을 하는 화면입니다.</p>
-            <button className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition">
-                가짜 로그인 버튼
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-green-600 mb-4">🔑 로그인 페이지</h1>
+      <p className="text-gray-600 mb-6">여기는 로그인을 하는 화면입니다.</p>
+      <button className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition">
+        가짜 로그인 버튼
+      </button>
+    </div>
+  );
 }
