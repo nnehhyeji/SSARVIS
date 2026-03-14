097335edfc0301b9beb26052af950bfe36da4dcc
김성훈 <sdzx0719@naver.com>
2026-03-15 00:55:51 +0900

    S14P21B203-188 #done [BE] 알림 구독

--- backend/src/main/java/com/ssafy/ssarvis/common/constant/Constants.java
+++ backend/src/main/java/com/ssafy/ssarvis/common/constant/Constants.java
@@ -5,4 +5,6 @@ public final class Constants {
     public static final String DEFAULT_PROFILE_IMAGE = "URL...";
 
     public static final String DEFAULT_PROVIDER = "KAKAO";
+
+    public static final String SSE_NOTIFICATION_CHANNEL = "sse:notification";
 }
