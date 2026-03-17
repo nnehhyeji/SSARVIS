5d69f8f94bd6d088ce5600ddcb3b66409271b7d2
cramfull <tmdgud99@gmail.com>
2026-03-17 14:06:10 +0900

    fix: S14P21B203-121 #progress [BE] 내 정보 조회

--- backend/src/main/java/com/ssafy/ssarvis/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/ssarvis/user/controller/UserController.java
@@ -12,7 +12,6 @@ import com.ssafy.ssarvis.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
