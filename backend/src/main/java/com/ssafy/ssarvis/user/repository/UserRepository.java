3a1e79134ea8f8bad81364fa4e1273bb32917682
cramfull <tmdgud99@gmail.com>
2026-03-17 14:17:20 +0900

    fix: S14P21B203-124 #progress [BE] 회원 가입

--- backend/src/main/java/com/ssafy/ssarvis/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/ssarvis/user/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.ssarvis.user.repository;
 
 import com.ssafy.ssarvis.user.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
