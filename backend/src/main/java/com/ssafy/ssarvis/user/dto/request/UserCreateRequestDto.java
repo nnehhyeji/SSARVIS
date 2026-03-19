367f928fa0416e9de4ab992fd228264590e29b14
cramfull <tmdgud99@gmail.com>
2026-03-19 09:54:37 +0900

    fix: S14P21B203-282 #done [BE] cors 설정 추가

--- backend/src/main/java/com/ssafy/ssarvis/user/dto/request/UserCreateRequestDto.java
+++ backend/src/main/java/com/ssafy/ssarvis/user/dto/request/UserCreateRequestDto.java
@@ -14,7 +14,7 @@ public record UserCreateRequestDto(
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     @Pattern(
         regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$",
-        message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다."
+        message = "비밀번호는 영문, 숫자를 포함해야 합니다."
     )
     String password,
 
