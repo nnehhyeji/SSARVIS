b0b016a5e5bf131aa85987799c18f14f93da4c20
김성훈 <sdzx0719@naver.com>
2026-03-23 12:41:36 +0900

    fix: 응답 생성 로직 수정

--- backend/src/main/java/com/ssafy/ssarvis/voice/repository/PromptRepository.java
+++ backend/src/main/java/com/ssafy/ssarvis/voice/repository/PromptRepository.java
@@ -5,9 +5,13 @@ import com.ssafy.ssarvis.voice.entity.PromptType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PromptRepository extends JpaRepository<Prompt, Long> {
 
     long countByUserIdAndPromptType(Long userId, PromptType promptType);
+
+    Optional<Prompt> findTopByUserIdAndPromptTypeOrderByIdDesc(Long userId, PromptType promptType);
 
 }
